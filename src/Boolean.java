2161405bc863384dc95a07aab57d8f33413e2d1f
Salohiddin00 <salohiddinabduganiyev869@gmail.com>
2025-02-13 19:00:04 +0500

    first commit

---
+++ src/Boolean.java
@@ -0,0 +1,429 @@
+import java.util.Scanner;
+
+public class Boolean {
+    public static void main(String[] args) {
+        // 1 misol
+        /*Scanner a=new Scanner(System.in);
+        System.out.print("A:");
+        int A= a.nextInt();
+        boolean res=(A>0);
+        System.out.println("Natija:"+res);*/
+
+        // 2 misol
+        /*Scanner a=new Scanner(System.in);
+        System.out.print("A:");
+        int A= a.nextInt();
+        boolean res=(A%2!=0);
+        System.out.println("Natija:"+res);*/
+
+        // 3 misol
+        /*Scanner a=new Scanner(System.in);
+        System.out.print("A:");
+        int A= a.nextInt();
+        boolean res=(A%2==0);
+        System.out.println("Natija:"+res);*/
+
+        // 4 misol
+        /*Scanner a=new Scanner(System.in);
+        System.out.print("A:");
+        int A= a.nextInt();
+        Scanner b=new Scanner(System.in);
+        System.out.print("B:");
+        int B= b.nextInt();
+        boolean res=(A>2 && B<=3);
+        System.out.println("Natija:"+res);*/
+
+        // 5 misol
+        /*Scanner a=new Scanner(System.in);
+        System.out.print("A:");
+        int A= a.nextInt();
+        Scanner b=new Scanner(System.in);
+        System.out.print("B:");
+        int B= b.nextInt();
+        boolean res=(A>=0 && B<-2);
+        System.out.println("Natija:"+res);*/
+
+        // 6 misol
+        /*Scanner a=new Scanner(System.in);
+        System.out.print("A:");
+        int A= a.nextInt();
+        Scanner b=new Scanner(System.in);
+        System.out.print("B:");
+        int B= b.nextInt();
+        Scanner c=new Scanner(System.in);
+        System.out.print("C:");
+        int C= c.nextInt();
+        boolean res=(A<=B && B<=C);
+        System.out.println("Natija:"+res);*/
+
+        // 7 misol
+        /*Scanner a=new Scanner(System.in);
+        System.out.print("A:");
+        int A= a.nextInt();
+        Scanner b=new Scanner(System.in);
+        System.out.print("B:");
+        int B= b.nextInt();
+        Scanner c=new Scanner(System.in);
+        System.out.print("C:");
+        int C= c.nextInt();
+        boolean res=(A<=B && B<=C);
+        System.out.println("Natija:"+res);*/
+
+        // 8 misol
+        /*Scanner a=new Scanner(System.in);
+        System.out.print("A:");
+        int A= a.nextInt();
+        Scanner b=new Scanner(System.in);
+        System.out.print("B:");
+        int B= b.nextInt();
+        boolean res=(A%2!=0 && B%2!=0);
+        System.out.println("Natija:"+res);*/
+
+        // 9 misol
+        /*Scanner a=new Scanner(System.in);
+        System.out.print("A:");
+        int A= a.nextInt();
+        Scanner b=new Scanner(System.in);
+        System.out.print("B:");
+        int B= b.nextInt();
+        boolean res=(A%2!=0 || B%2!=0);
+        System.out.println("Natija:"+res);*/
+
+        // 10 misol
+        /*Scanner a=new Scanner(System.in);
+        System.out.print("A:");
+        int A= a.nextInt();
+        Scanner b=new Scanner(System.in);
+        System.out.print("B:");
+        int B= b.nextInt();
+        boolean res=((A%2==0 && B%2!=0)||(A%2!=0 && B%2==0));
+        System.out.println("Natija:"+res);*/
+
+        // 11 misol
+        /*Scanner a=new Scanner(System.in);
+        System.out.print("A:");
+        int A= a.nextInt();
+        Scanner b=new Scanner(System.in);
+        System.out.print("B:");
+        int B= b.nextInt();
+        boolean res=((A%2==0 && B%2==0)||(A%2!=0 && B%2!=0));
+        System.out.println("Natija:"+res);*/
+
+        //12 misol
+        /*Scanner a=new Scanner(System.in);
+        System.out.print("A:");
+        int A= a.nextInt();
+        Scanner b=new Scanner(System.in);
+        System.out.print("B:");
+        int B= b.nextInt();
+        Scanner c=new Scanner(System.in);
+        System.out.print("C:");
+        int C= c.nextInt();
+        boolean res=(A>0 && B>0 && C>0);
+        System.out.println("Natija:"+res);*/
+
+        // 13 misol
+        /*Scanner a=new Scanner(System.in);
+        System.out.print("A:");
+        int A= a.nextInt();
+        Scanner b=new Scanner(System.in);
+        System.out.print("B:");
+        int B= b.nextInt();
+        Scanner c=new Scanner(System.in);
+        System.out.print("C:");
+        int C= c.nextInt();
+        boolean res=(A>0 || B>0 || C>0);
+        System.out.println("Natija:"+res);*/
+
+        // 14 misol
+        /*Scanner a=new Scanner(System.in);
+        System.out.print("A:");
+        int A= a.nextInt();
+        Scanner b=new Scanner(System.in);
+        System.out.print("B:");
+        int B= b.nextInt();
+        Scanner c=new Scanner(System.in);
+        System.out.print("C:");
+        int C= c.nextInt();
+        boolean res=((A>0 && B<0 && C<0) || (A>0 && B<0 && C>0) ||(A>0 && B>0 && C<0));
+        System.out.println("Natija:"+res);*/
+
+        // 15 misol
+        /*Scanner a=new Scanner(System.in);
+        System.out.print("A:");
+        int A= a.nextInt();
+        Scanner b=new Scanner(System.in);
+        System.out.print("B:");
+        int B= b.nextInt();
+        Scanner c=new Scanner(System.in);
+        System.out.print("C:");
+        int C= c.nextInt();
+        boolean res=((A>0 && B>0 && C<0) || (A<0 && B>0 && C>0) ||(A>0 && B<0 && C>0));
+        System.out.println("Natija:"+res);*/
+
+        // 16 misol
+        /*Scanner a=new Scanner(System.in);
+        System.out.print("A:");
+        int A= a.nextInt();
+        boolean res=(A>=10 && A<=99 && A%2==0);
+        System.out.println("Natija:"+res);*/
+
+        // 17 misol
+        /*Scanner a=new Scanner(System.in);
+        System.out.print("A:");
+        int A= a.nextInt();
+        boolean res=(A>=100 && A<=999 && A%2!=0);
+        System.out.println("Natija:"+res);*/
+
+        // 18 misol
+        /*Scanner a=new Scanner(System.in);
+        System.out.print("A:");
+        int A= a.nextInt();
+        Scanner b=new Scanner(System.in);
+        System.out.print("B:");
+        int B= b.nextInt();
+        Scanner c=new Scanner(System.in);
+        System.out.print("C:");
+        int C= c.nextInt();
+        boolean res=(A==B || B==C || A==C);
+        System.out.println("Natija:"+res);*/
+
+        // 19 misol
+        /*Scanner a=new Scanner(System.in);
+        System.out.print("A:");
+        int A= a.nextInt();
+        Scanner b=new Scanner(System.in);
+        System.out.print("B:");
+        int B= b.nextInt();
+        Scanner c=new Scanner(System.in);
+        System.out.print("C:");
+        int C= c.nextInt();
+        boolean res=(A==-B || A==-C || B==-C);
+        System.out.println("Natija:"+res);*/
+
+        // 20 misol
+        /*Scanner a=new Scanner(System.in);
+        System.out.print("A:");
+        int A= a.nextInt();
+        boolean res=(A>=100 && A<=999 && A%10!=(A/10)%10 && A%10!=A/100 && (A/10)%10!=A/100);
+        System.out.println("Natija:"+res);*/
+
+        // 21 misol
+        /*Scanner a=new Scanner(System.in);
+        System.out.print("A:");
+        int A= a.nextInt();
+        boolean res=(A>=100 && A<=999 && A%10>(A/10)%10 && A%10>A/100 && (A/10)%10>A/100);
+        System.out.println("Natija:"+res);*/
+
+        // 22 misol
+        /*Scanner a=new Scanner(System.in);
+        System.out.print("A:");
+        int A= a.nextInt();
+        boolean res=(A>=100 && A<=999 && ((A%10>(A/10)%10 && A%10>A/100 && (A/10)%10>A/100) ||(A%10<(A/10)%10 && A%10<A/100 && (A/10)%10<A/100)));
+        System.out.println("Natija:"+res);*/
+
+        // 23 misol
+        /*Scanner a=new Scanner(System.in);
+        System.out.print("A:");
+        int A= a.nextInt();
+        boolean res=(A>=100 && A<=999 && A/100==A%10);
+        System.out.println("Natija:"+res);*/
+
+        // 24 misol
+        /*Scanner a=new Scanner(System.in);
+        System.out.print("A:");
+        int A= a.nextInt();
+        Scanner b=new Scanner(System.in);
+        System.out.print("B:");
+        int B= b.nextInt();
+        Scanner c=new Scanner(System.in);
+        System.out.print("C:");
+        int C= c.nextInt();
+        double D=B*B-(4*A*C);
+        boolean res=(D>=0);
+        System.out.println("Natija:"+res);*/
+
+        // 25 misol
+        /*Scanner a=new Scanner(System.in);
+        System.out.print("x:");
+        int x= a.nextInt();
+        Scanner b=new Scanner(System.in);
+        System.out.print("y:");
+        int y= b.nextInt();
+        boolean res=(y>0 && x<0);
+        System.out.println("Natija:"+res);*/
+
+        // 26 misol
+        /*Scanner a=new Scanner(System.in);
+        System.out.print("x:");
+        int x= a.nextInt();
+        Scanner b=new Scanner(System.in);
+        System.out.print("y:");
+        int y= b.nextInt();
+        boolean res=(y<0 && x<0);
+        System.out.println("Natija:"+res);*/
+
+        // 27 misol
+        /*Scanner a=new Scanner(System.in);
+        System.out.print("x:");
+        int x= a.nextInt();
+        Scanner b=new Scanner(System.in);
+        System.out.print("y:");
+        int y= b.nextInt();
+        boolean res=(x<0);
+        System.out.println("Natija:"+res);*/
+
+        // 28 misol
+        /*Scanner a=new Scanner(System.in);
+        System.out.print("x:");
+        int x= a.nextInt();
+        Scanner b=new Scanner(System.in);
+        System.out.print("y:");
+        int y= b.nextInt();
+        boolean res=((y>0 && x>0) || (y<0 && x<0));
+        System.out.println("Natija:"+res);*/
+
+        // 29 misol
+
+
+        // 30 misol
+        /*Scanner a=new Scanner(System.in);
+        System.out.print("A:");
+        int A= a.nextInt();
+        Scanner b=new Scanner(System.in);
+        System.out.print("B:");
+        int B= b.nextInt();
+        Scanner c=new Scanner(System.in);
+        System.out.print("C:");
+        int C= c.nextInt();
+        boolean res=(A==B && B==C);
+        System.out.println("Res:"+res);*/
+
+        // 31 misol
+        /*Scanner a=new Scanner(System.in);
+        System.out.print("A:");
+        int A= a.nextInt();
+        Scanner b=new Scanner(System.in);
+        System.out.print("B:");
+        int B= b.nextInt();
+        Scanner c=new Scanner(System.in);
+        System.out.print("C:");
+        int C= c.nextInt();
+        boolean res=(A==B || B==C || A==C);
+        System.out.println("Res:"+res);*/
+
+        // 32 misol
+        /*Scanner a=new Scanner(System.in);
+        System.out.print("A:");
+        int A= a.nextInt();
+        Scanner b=new Scanner(System.in);
+        System.out.print("B:");
+        int B= b.nextInt();
+        Scanner c=new Scanner(System.in);
+        System.out.print("C:");
+        int C= c.nextInt();
+        boolean res=(A*A+B*B==C*C || A*A+C*C==B*B || C*C+B*B==A*A);
+        System.out.println("Res:"+res);*/
+
+        // 33 misol
+        /*Scanner a=new Scanner(System.in);
+        System.out.print("A:");
+        int A= a.nextInt();
+        Scanner b=new Scanner(System.in);
+        System.out.print("B:");
+        int B= b.nextInt();
+        Scanner c=new Scanner(System.in);
+        System.out.print("C:");
+        int C= c.nextInt();
+        boolean res=(A+B>C && B+C>A && A+C>B);
+        System.out.println("Res:"+res);*/
+
+        // 34 MISOL
+        /*Scanner a=new Scanner(System.in);
+        System.out.print("x:");
+        int A= a.nextInt();
+        Scanner b=new Scanner(System.in);
+        System.out.print("y:");
+        int B= b.nextInt();
+        boolean res=(A%2==0 && B%2==0);
+        System.out.println("Res:"+res);*/
+
+        // 35 misol
+        /*Scanner a=new Scanner(System.in);
+        System.out.print("x1:");
+        int A= a.nextInt();
+        Scanner b=new Scanner(System.in);
+        System.out.print("y1:");
+        int B= b.nextInt();
+        Scanner a1=new Scanner(System.in);
+        System.out.print("x2:");
+        int A1= a1.nextInt();
+        Scanner b1=new Scanner(System.in);
+        System.out.print("y2:");
+        int B1= b1.nextInt();
+        boolean res=((((A+B)%2==0) && ((A1+B1)%2==0)) || (((A+B)%2!=0) && ((A1+B1)%2!=0)));
+        System.out.println("Res:"+res);*/
+
+        // 36 misol
+        /*Scanner a=new Scanner(System.in);
+        System.out.print("x1:");
+        int A= a.nextInt();
+        Scanner b=new Scanner(System.in);
+        System.out.print("y1:");
+        int B= b.nextInt();
+        Scanner a1=new Scanner(System.in);
+        System.out.print("x2:");
+        int A1= a1.nextInt();
+        Scanner b1=new Scanner(System.in);
+        System.out.print("y2:");
+        int B1= b1.nextInt();
+        boolean res=(A==A1 || B==B1);
+        System.out.println("Res:"+res);*/
+
+        // 37 misol
+        /*Scanner a=new Scanner(System.in);
+        System.out.print("x1:");
+        int A= a.nextInt();
+        Scanner b=new Scanner(System.in);
+        System.out.print("y1:");
+        int B= b.nextInt();
+        Scanner a1=new Scanner(System.in);
+        System.out.print("x2:");
+        int A1= a1.nextInt();
+        Scanner b1=new Scanner(System.in);
+        System.out.print("y2:");
+        int B1= b1.nextInt();
+        boolean res=(A+1==A1 || B+1==B1 || A1+1==A || B1+1==B);
+        System.out.println("Res:"+res);*/
+
+        // 38 misol
+        Scanner a=new Scanner(System.in);
+        System.out.print("x1:");
+        int A= a.nextInt();
+        Scanner b=new Scanner(System.in);
+        System.out.print("y1:");
+        int B= b.nextInt();
+        Scanner a1=new Scanner(System.in);
+        System.out.print("x2:");
+        int A1= a1.nextInt();
+        Scanner b1=new Scanner(System.in);
+        System.out.print("y2:");
+        int B1= b1.nextInt();
+        boolean res=((A==B && A1==B1)||(Math.abs(A-A1)==Math.abs(B-B1)));
+        System.out.println("Res:"+res);
+
+        // 39 misol
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/Integer.java
@@ -0,0 +1,225 @@
+public class Integer {
+    public static void main(String[] args) {
+
+        // 1 misol
+        /*Scanner L1=new Scanner(System.in);
+        System.out.print("L:");
+        int L=(int) L1.nextDouble();
+        int M=L/100;
+        System.out.println("M:"+M);*/
+
+        // 2 misol
+        /*Scanner L1=new Scanner(System.in);
+        System.out.print("KG:");
+        int M=L1.nextINT();
+        int T=M/1000;
+        System.out.println("tonna:"+T);*/
+
+        // 3 misol
+        /*Scanner L1=new Scanner(System.in);
+        System.out.print("bayt:");
+        int bayt=L1.nextInt();
+        int M=bayt/1024;
+        System.out.println("Kb:"+M);*/
+
+        // 4 misol
+        /*Scanner L1=new Scanner(System.in);
+        System.out.print("A>B  A:");
+        int A=L1.nextInt();
+        Scanner b1=new Scanner(System.in);
+        System.out.print("A>B  B:");
+        int B=b1.nextInt();
+        int N=A/B;
+        System.out.println("Nechta:"+N);*/
+
+        // 5 misol
+        /*Scanner L1=new Scanner(System.in);
+        System.out.print("A>B  A:");
+        int A=L1.nextInt();
+        Scanner b1=new Scanner(System.in);
+        System.out.print("A>B  B:");
+        int B=b1.nextInt();
+        int N=A/B;
+        System.out.println("Nechta:"+N);
+        int Q=A%B;
+        System.out.println("Qoldiq:"+Q);*/
+
+        // 6 misol
+        /*Scanner L1=new Scanner(System.in);
+        System.out.print("A:");
+        int A=L1.nextInt();
+        int yuz=(A/10)%10;
+        System.out.println("O'nlik:"+yuz);
+        int on=A%10;
+        System.out.println("Birlik:"+on);*/
+
+        // 7 misol
+        /*Scanner L1=new Scanner(System.in);
+        System.out.print("A:");
+        int A=L1.nextInt();
+        int yuz=(A/10)%10;
+        System.out.println("O'nlik:"+yuz);
+        int on=A%10;
+        System.out.println("Birlik:"+on);
+        int sum=on+yuz;
+        System.out.println("Raqamlar yig'indisi"+sum);*/
+
+        // 8 misol
+        /*Scanner L1=new Scanner(System.in);
+        System.out.print("A:");
+        int A=L1.nextInt();
+        int yuz=(A/10)%10;
+        int on=A%10;
+        int res=on*10+yuz;
+        System.out.println("Teskari:"+res);*/
+
+        // 9 misol
+        /*Scanner L1=new Scanner(System.in);
+        System.out.print("A:");
+        int A=L1.nextInt();
+        int yuz=(A/100)%10;
+        System.out.println("Yuz:"+yuz);*/
+
+        // 10 misol
+        /*Scanner L1=new Scanner(System.in);
+        System.out.print("A:");
+        int A=L1.nextInt();
+        int bir=A%10;
+        System.out.println("Birliklar:"+bir);
+        int on=(A/10)%10;
+        System.out.println("O'nliklar"+on);*/
+
+        // 11 misol
+        /*Scanner L1=new Scanner(System.in);
+        System.out.print("A:");
+        int A=L1.nextInt();
+        int bir=A%10;
+        int on=(A/10)%10;
+        int yuz=A/100;
+        int sum=bir+on+yuz;
+        System.out.println("Raqamlar Yig'indisi:"+sum);*/
+
+        // 12 misol
+        /*Scanner L1=new Scanner(System.in);
+        System.out.print("A:");
+        int A=L1.nextInt();
+        int bir=A%10;
+        int on=(A/10)%10;
+        int yuz=A/100;
+        int sum=bir*100+on*10+yuz;
+        System.out.println("Raqamlar Yig'indisi:"+sum);*/
+
+        // 13 misol
+        /*Scanner L1=new Scanner(System.in);
+        System.out.print("A:");
+        int A=L1.nextInt();
+        int bir=A%10;
+        int on=(A/10)%10;
+        int yuz=A/100;
+        int sum=bir*10+on*100+yuz;
+        System.out.println("Raqamlar Yig'indisi:"+sum);*/
+
+        // 14 misol
+        /*Scanner L1=new Scanner(System.in);
+        System.out.print("A:");
+        int A=L1.nextInt();
+        int bir=A%10;
+        int on=(A/10)%10;
+        int yuz=A/100;
+        int sum=bir*100+on+yuz*10;
+        System.out.println("Raqamlar Yig'indisi:"+sum);*/
+
+        // 15 misol
+        /*Scanner L1=new Scanner(System.in);
+        System.out.print("A:");
+        int A=L1.nextInt();
+        int bir=A%10;
+        int on=(A/10)%10;
+        int yuz=A/100;
+        int sum=bir+on*100+yuz*10;
+        System.out.println("Raqamlar Yig'indisi:"+sum);*/
+
+        // 16 misol
+        /*Scanner L1=new Scanner(System.in);
+        System.out.print("A:");
+        int A=L1.nextInt();
+        int bir=A%10;
+        int on=(A/10)%10;
+        int yuz=A/100;
+        int sum=bir*10+on+yuz*100;
+        System.out.println("Raqamlar Yig'indisi:"+sum);*/
+
+        // 17 misol
+        /*Scanner L1=new Scanner(System.in);
+        System.out.print("A:");
+        int A=L1.nextInt();
+        int yuz=(A/100)%10;
+        System.out.println(Yuzlikalr:"+yuz);*/
+
+        // 18 misol
+        /*Scanner L1=new Scanner(System.in);
+        System.out.print("A:");
+        int A=L1.nextInt();
+        int ming=(A/1000)%10;
+        System.out.println("Mingliklar:"+ming);*/
+
+        // 19 misol
+        /*Scanner Sek=new Scanner(System.in);
+        System.out.println("Sekund:");
+        int Sekund= Sek.nextInt();
+        int minut=Sekund/60;
+        System.out.println("Minut:"+minut);*/
+
+        // 20 misol
+        /*Scanner Sek=new Scanner(System.in);
+        System.out.println("Sekund:");
+        int Sekund= Sek.nextInt();
+        int soat=Sekund/3600;
+        System.out.println("soat:"+soat);*/
+
+        // 21 misol
+        /*Scanner Sek=new Scanner(System.in);
+        System.out.println("Sekund:");
+        int Sekund= Sek.nextInt();
+        int minut=Sekund/60;
+        int qoldiq=Sekund%60;
+        System.out.println("Minut:"+minut);
+        System.out.println("Sekund:"+qoldiq);*/
+
+        // 22 misol
+        /*Scanner Sek=new Scanner(System.in);
+        System.out.println("Sekund:");
+        int Sekund= Sek.nextInt();
+        int soat=Sekund/3600;
+        int qoldiq=Sekund%60;
+        System.out.println("soat:"+soat);
+        System.out.println("Sekund:"+qoldiq);*/
+
+        // 23 misol
+        /*Scanner Sek=new Scanner(System.in);
+        System.out.println("Sekund:");
+        int Sekund= Sek.nextInt();
+        int soat=Sekund/3600;
+        int minut=Sekund/60;
+        int qoldiq2=minut%60;
+        int qoldiq=Sekund%60;
+        System.out.println("soat:"+soat);
+        System.out.println("Minut:"+qoldiq2);
+        System.out.println("Sekund:"+qoldiq);*/
+
+        // 24 misol
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
